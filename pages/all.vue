3424d6ea4f57060d6089ba11fa033ebb81fb7ef9
Naoko3in4 <okashimochi@gimail.com>
2020-05-10 15:26:04 +0900

    setting colors

--- pages/all.vue
+++ pages/all.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container all__wrapper">
-    <h2>登録単語一覧</h2>
+    <h2 class="list-title">登録単語一覧</h2>
     <div class="all_word_list__wrapper">
       <ul
         v-for="content in contents"
         :key="content.sys.id"
         class="all_word-list"
       >
-        <li>{{ content.fields.word }}:{{ content.fields.meaning }}</li>
+        <li class="word__wrapper">
+          <span class="registerd_word">{{ content.fields.word }}</span
+          ><span class="registerd_meaning">:{{ content.fields.meaning }}</span>
+        </li>
       </ul>
     </div>
 
@@ -41,10 +44,37 @@ export default {
 
 .all_word_list__wrapper {
   margin: 0 auto;
-  max-width: 200px;
+  max-width: 350px;
+  margin-bottom: 30px;
+  background: #fff;
+  padding: 20px;
+  padding-bottom: 35px;
+  border-radius: 9px;
 }
 
 .all_word-list {
   text-align: left;
+}
+
+.list-title {
+  font-weight: 700;
+  font-size: 20px;
+  color: #35495e;
+  letter-spacing: 1px;
+  margin-bottom: 15px;
+}
+
+.word__wrapper {
+  line-height: 30px;
+  border-bottom: 1px solid #d6d7d8;
+}
+
+.registerd_word {
+  font-weight: 700;
+  color: #0a6095;
+}
+
+.registerd_meaning {
+  font-size: 14px;
 }
 </style>
